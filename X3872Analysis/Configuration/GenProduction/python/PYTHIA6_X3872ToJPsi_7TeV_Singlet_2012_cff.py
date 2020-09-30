cff0e0519dde0ebf1880b242f86237066b8cc4ec
xuyan <xlyan0636@gmail.com>
2020-09-30 11:19:56 +0200

    Codes for 2011

--- X3872Analysis/Configuration/GenProduction/python/PYTHIA6_X3872ToJPsi_7TeV_Singlet_2012_cff.py
+++ X3872Analysis/Configuration/GenProduction/python/PYTHIA6_X3872ToJPsi_7TeV_Singlet_2012_cff.py
@@ -5,7 +5,6 @@
 # with command line options: Configuration/GenProduction/python/PYTHIA6_Psi2SToJPsi_7TeV_cff.py -s GEN,SIM,DIGI,L1,DIGI2RAW,RAW2DIGI,RECO --eventcontent FEVTSIM --conditions START37_V5::All --datatier GEN-SIM-RAW-RECO -n 10 --no_exec
 import FWCore.ParameterSet.Config as cms
 
-
 configurationMetadata = cms.untracked.PSet(
     version = cms.untracked.string('$Revision: 1.1 $'),
     annotation = cms.untracked.string('September 2010: Pythia6 generation of prompt X3872, 7TeV, withEvtGen'),
@@ -17,41 +16,37 @@ from Configuration.Generator.PythiaUEZ2Settings_cfi import * #MSTP(81)=1, 21 pro
 
 # Input source
 source = cms.Source("EmptySource")
-
-
-
 generator = cms.EDFilter("Pythia6GeneratorFilter",
     pythiaPylistVerbosity = cms.untracked.int32(0),
     filterEfficiency = cms.untracked.double(0.0205),
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     comEnergy = cms.double(7000.0),
-                         crossSection = cms.untracked.double(2781000.0),  #Pythia Value, not corrected
+    crossSection = cms.untracked.double(2781000.0),  #Pythia Value, not corrected
     maxEventsToPrint = cms.untracked.int32(0),
 
-
     ExternalDecays = cms.PSet(
-    EvtGen = cms.untracked.PSet(
-         operates_on_particles = cms.vint32(0), # 0 (zero) means default list (hardcoded) 20443
+        EvtGen = cms.untracked.PSet(
+            operates_on_particles = cms.vint32(0), # 0 (zero) means default list (hardcoded) 20443
                                                   # you can put here the list of particles (PDG IDs)
                                                   # that you want decayed by EvtGen
-         use_default_decay = cms.untracked.bool(False),
-         decay_table = cms.FileInPath('GeneratorInterface/ExternalDecays/data/DECAY_NOLONGLIFE.DEC'),
-         particle_property_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/evt.pdl'),
-         user_decay_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/Onia_mumu.dec'),
-         list_forced_decays = cms.vstring('myX3872'),
-     ),
+            use_default_decay = cms.untracked.bool(False),
+            decay_table = cms.FileInPath('GeneratorInterface/ExternalDecays/data/DECAY_NOLONGLIFE.DEC'),
+            particle_property_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/evt.pdl'),
+            user_decay_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/Onia_mumu.dec'),
+            list_forced_decays = cms.vstring('myX3872'),
+        ),
         parameterSets = cms.vstring('EvtGen')
     ),
 
     
     PythiaParameters = cms.PSet(
         pythiaUESettingsBlock,
-        processParameters = cms.vstring('MSEL=0          ! Free choice',
-            'MSUB(88) = 1  !Activate gg->chi_1_c+g',
-            'PMAS(289,1) = 3.87156 !Change chi_1_c mass to X3872',
-            'PMAS(289,2) = 0.0023 !Change chi_c1 width to X3872' ,
-
-                                        
+        processParameters = cms.vstring(
+            'MSEL=0                 ! Free choice',
+            'MSUB(88) = 1           !Activate gg->chi_1_c+g',
+            'PMAS(289,1) = 3.87156  !Change chi_1_c mass to X3872',
+            'PMAS(289,2) = 0.0023   !Change chi_c1 width to X3872' ,
+ 
             'MSTP(142)=2      ! turns on the PYEVWT Pt re-weighting routine', 
 
             'PARJ(13)=0.750   ! probability that a c or b meson has S=1', 
@@ -74,17 +69,16 @@ mumugenfilter = cms.EDFilter("MCParticlePairFilter",
     MaxEta = cms.untracked.vdouble(2.5, 2.5),
     MinEta = cms.untracked.vdouble(-2.5, -2.5),
     ParticleCharge = cms.untracked.int32(-1),
-    MinPt = cms.untracked.vdouble(2.5, 2.5),
+    # MinPt = cms.untracked.vdouble(2.5, 2.5),
     ParticleID1 = cms.untracked.vint32(13),
     ParticleID2 = cms.untracked.vint32(13)
 )
 
-
 oniafilter = cms.EDFilter("PythiaFilter",
-    Status = cms.untracked.int32(2),
-    MaxRapidity = cms.untracked.double(1.4),
-    MinRapidity = cms.untracked.double(-1.4),
-    MinPt = cms.untracked.double(10.0),
+    Status = cms.untracked.int32(2),   
+    MaxRapidity = cms.untracked.double(2.2),
+    MinRapidity = cms.untracked.double(-2.2),
+    MinPt = cms.untracked.double(2.0),
     ParticleID = cms.untracked.int32(20443)
 )
 
